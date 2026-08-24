c2e0734e027ad1e1ccd3209d84105b6965e60de2
kiselkov0809-hub <kiselkov0809@gmail.com>
2026-08-24 19:34:18 +0300

    server.js

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
-const WebSocket = require('ws');
-
+                const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 let rooms = {};
 let matchmaking = [];
 
-wss.on('connection', (ws) => {
-    ws.id = Date.now() + Math.random();
-    
-    ws.on('message', (msg) => {
+wss.on('connection', function(ws) {
+    ws.on('message', function(msg) {
         try {
             const data = JSON.parse(msg);
             handleMessage(ws, data);
         } catch(e) {}
     });
     
-    ws.on('close', () => {
-        matchmaking = matchmaking.filter(m => m.ws !== ws);
+    ws.on('close', function() {
+        matchmaking = matchmaking.filter(function(m) { return m.ws !== ws; });
         for (let roomId in rooms) {
-            const room = rooms[roomId];
-            if (room.p1 === ws || room.p2 === ws) {
-                const opponent = room.p1 === ws ? room.p2 : room.p1;
-                if (opponent) {
-                    opponent.send(JSON.stringify({ type: 'opponent_left' }));
-                }
+            if (rooms[roomId].p1 === ws || rooms[roomId].p2 === ws) {
+                const opponent = rooms[roomId].p1 === ws ? rooms[roomId].p2 : rooms[roomId].p1;
+                if (opponent) opponent.send(JSON.stringify({ type: 'opponent_left' }));
                 delete rooms[roomId];
             }
         }
@@ -31,71 +25,48 @@ wss.on('connection', (ws) => {
 });
 
 function handleMessage(ws, data) {
-    switch(data.type) {
-        case 'join_queue':
-            matchmaking.push({ ws, nick: data.nick });
-            ws.send(JSON.stringify({ type: 'queue_joined' }));
-            findMatch(ws);
-            break;
-            
-        case 'leave_queue':
-            matchmaking = matchmaking.filter(m => m.ws !== ws);
-            break;
-            
-        case 'move':
-            const room1 = findRoom(ws);
-            if (room1) {
-                const opponent = room1.p1 === ws ? room1.p2 : room1.p1;
-                if (opponent) {
-                    opponent.send(JSON.stringify({ type: 'opponent_move', x: data.x }));
-                }
-            }
-            break;
-            
-        case 'attack':
-            const room2 = findRoom(ws);
-            if (room2) {
-                const opponent = room2.p1 === ws ? room2.p2 : room2.p1;
-                if (opponent) {
-                    opponent.send(JSON.stringify({ type: 'opponent_attack', dmg: data.dmg }));
-                }
-            }
-            break;
-            
-        case 'game_over':
-            const room3 = findRoom(ws);
-            if (room3) {
-                const opponent = room3.p1 === ws ? room3.p2 : room3.p1;
-                if (opponent) {
-                    opponent.send(JSON.stringify({ type: 'game_result', win: data.win }));
-                }
-                delete rooms[room3.id];
-            }
-            break;
-    }
-}
-
-function findMatch(ws) {
-    if (matchmaking.length >= 2) {
-        const p1 = matchmaking.shift();
-        const p2 = matchmaking.shift();
-        
-        const roomId = Date.now() + Math.random();
-        const room = { id: roomId, p1: p1.ws, p2: p2.ws, nick1: p1.nick, nick2: p2.nick };
-        rooms[roomId] = room;
-        
-        p1.ws.send(JSON.stringify({ type: 'match_found', opponent: p2.nick, side: 'left', roomId }));
-        p2.ws.send(JSON.stringify({ type: 'match_found', opponent: p1.nick, side: 'right', roomId }));
-    }
-}
-
-function findRoom(ws) {
-    for (let roomId in rooms) {
-        if (rooms[roomId].p1 === ws || rooms[roomId].p2 === ws) {
-            return rooms[roomId];
+    if (data.type === 'join_queue') {
+        matchmaking.push({ ws: ws, nick: data.nick, character: data.character || 'melstroy' });
+        ws.send(JSON.stringify({ type: 'queue_joined' }));
+        if (matchmaking.length >= 2) {
+            const p1 = matchmaking.shift();
+            const p2 = matchmaking.shift();
+            const roomId = Date.now();
+            rooms[roomId] = { p1: p1.ws, p2: p2.ws, nick1: p1.nick, nick2: p2.nick };
+            p1.ws.send(JSON.stringify({ type: 'match_found', opponent: p2.nick, side: 'left', opponentCharacter: p2.character }));
+            p2.ws.send(JSON.stringify({ type: 'match_found', opponent: p1.nick, side: 'right', opponentCharacter: p1.character }));
         }
     }
-    return null;
+    
+    if (data.type === 'move') {
+        for (let roomId in rooms) {
+            const room = rooms[roomId];
+            if (room.p1 === ws) room.p2.send(JSON.stringify({ type: 'opponent_move', x: data.x }));
+            if (room.p2 === ws) room.p1.send(JSON.stringify({ type: 'opponent_move', x: data.x }));
+        }
+    }
+    
+    if (data.type === 'attack') {
+        for (let roomId in rooms) {
+            const room = rooms[roomId];
+            if (room.p1 === ws) room.p2.send(JSON.stringify({ type: 'opponent_attack', dmg: data.dmg }));
+            if (room.p2 === ws) room.p1.send(JSON.stringify({ type: 'opponent_attack', dmg: data.dmg }));
+        }
+    }
+    
+    if (data.type === 'game_over') {
+        for (let roomId in rooms) {
+            const room = rooms[roomId];
+            if (room.p1 === ws) {
+                room.p2.send(JSON.stringify({ type: 'game_result', win: false }));
+                delete rooms[roomId];
+            }
+            if (room.p2 === ws) {
+                room.p1.send(JSON.stringify({ type: 'game_result', win: false }));
+                delete rooms[roomId];
+            }
+        }
+    }
 }
 
-console.log('WebSocket server running on port ' + (process.env.PORT || 8080));
+console.log('Server running');
