235a6a6939fdb51505d0119a1174aa943ae2d05b
kiselkov0809-hub <kiselkov0809@gmail.com>
2026-08-26 01:52:56 +0400

    server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-              const WebSocket = require('ws');
+                          const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: process.env.PORT || 8080 });
 
 let rooms = {};
